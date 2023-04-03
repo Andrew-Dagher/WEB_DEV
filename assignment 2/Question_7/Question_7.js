0432df941558e1a8c53b3f88f4c12fec8141c4f5
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-04-03 13:11:58 -0400

    287

--- assignment 2/Question_7/Question_7.js
+++ assignment 2/Question_7/Question_7.js
@@ -8,82 +8,12 @@ function updateTime() {
 setInterval(updateTime, 1000);
 
 
-//dog form for find dog/cat
-function validateForm1() {
-  var breedInput1 = document.forms["Find_a_Dog"]["Breed1"].value;
-  var doesNotMatterCheckbox1 = document.forms["Find_a_Dog"]["DoesNotMatter1"];
-  var dropdownAge1 = document.forms["Find_a_Dog"]["age1"];
-  var dropdownGender1 = document.forms["Find_a_Dog"]["gender1"];
+function validateCheckboxes() {
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  var checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
 
-  if (doesNotMatterCheckbox1.checked) {
-    if (dropdownAge1.value === "default") {
-      alert("Please select an option from the dropdown menu.");
-      event.preventDefault();
-      return false;
-    }
-    if (dropdownGender1.value === "default") {
-      alert("Please select an option from the dropdown menu.");
-      event.preventDefault();
-      return false;
-    }
+  if (!checkedOne) {
+    alert("Please select at least one checkbox.");
   }
-  
-  if (!doesNotMatterCheckbox1.checked && breedInput1 === "") {
-    alert("Please input a breed");
-    event.preventDefault();
-    return false;
-  }
-  if (dropdownAge1.value === "default") {
-    alert("Please select an option from the dropdown menu.");
-    event.preventDefault();
-    return false;
-  }
-  if (dropdownGender1.value === "default") {
-    alert("Please select an option from the dropdown menu.");
-    event.preventDefault();
-    return false;
-  }
-
-  return true;
 }
-
-//cat form for find dog/cat
-function validateForm2() {
-  var breedInput2 = document.forms["Find_a_Cat"]["Breed2"].value;
-  var doesNotMatterCheckbox2 = document.forms["Find_a_Cat"]["DoesNotMatter2"];
-  var dropdownAge2 = document.forms["Find_a_Cat"]["age2"];
-  var dropdownGender2 = document.forms["Find_a_Cat"]["gender2"];
-
-  if (doesNotMatterCheckbox2.checked) {
-    if (dropdownAge2.value === "default") {
-      alert("Please select an option from the dropdown menu.");
-      event.preventDefault();
-      return false;
-    }
-    if (dropdownGender2.value === "default") {
-      alert("Please select an option from the dropdown menu.");
-      event.preventDefault();
-      return false;
-    }
-  }
-  if (!doesNotMatterCheckbox2.checked && breedInput2 === "") {
-    alert("Please input a breed");
-    event.preventDefault();
-    return false;
-  }
-  if (dropdownAge2.value === "default") {
-    alert("Please select an option from the dropdown menu.");
-    event.preventDefault();
-    return false;
-  }
-  if (dropdownGender2.value === "default") {
-    alert("Please select an option from the dropdown menu.");
-    event.preventDefault();
-    return false;
-  }
-
-  return true;
-}
-
-
 
